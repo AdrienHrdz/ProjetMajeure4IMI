5c19401cdb1db0759cd7fc952f2396bde8da23fe
AdrienHrdz <adrienhernandez16@gmail.com>
2022-05-31 17:18:51 +0200

    fin de tournage

--- src/diametre.py
+++ src/diametre.py
@@ -3,16 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from divers import LECTURE_IMAGE
 
-filename = './aruco./IMG_20220531_144812.jpg'
+filename = './aruco./IMG_20220531_170740.jpg'
 
 IMAGE = LECTURE_IMAGE(filename)
-plt.figure()
-plt.subplot(121)
-plt.imshow(cv2.cvtColor(IMAGE,cv2.COLOR_BGR2RGB))
+IMAGE = cv2.cvtColor(IMAGE, cv2.COLOR_RGB2GRAY)
 
 ret,thresh1=cv2.threshold(IMAGE,125,255,cv2.THRESH_BINARY_INV)
-element_structurant = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
-IMAGE = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, element_structurant)
-plt.subplot(122)
-plt.imshow(cv2.cvtColor(IMAGE,cv2.COLOR_BGR2RGB))
+bweuler, array_components = cv2.connectedComponents(IMAGE.astype(np.uint8))
+print(bweuler)
+plt.imshow(thresh1, cmap='gray')
 plt.show()
+
+for n in range(10, 200, 10):
+    bweuler1 = cv2.connectedComponents(IMAGE.astype(np.uint8))[0]
+    element_structurant = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(n,n))
+    IMAGE_OPEN = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, element_structurant)
+    bweuler2 = cv2.connectedComponents(IMAGE_OPEN.astype(np.uint8))[0]
+    print(n, bweuler1, bweuler2)
+
