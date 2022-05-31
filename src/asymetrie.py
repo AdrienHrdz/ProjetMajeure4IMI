4159eb4826062ac8b377d5e2478770e972f4c316
Teklaw7 <timothee.teyssier@cpe.fr>
2022-05-31 12:08:33 +0200

    bon app

--- src/asymetrie.py
+++ src/asymetrie.py
@@ -1,5 +1,22 @@
+from re import I
 import cv2, platform,time
+from cv2 import waitKey
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.image as mpimg
 
-I=cv2.imread()
+
+
+I=cv2.imread('data/gdb_benin.jpg',0)#.astype(float)
+
+plt.imshow(cv2.cvtColor(I,cv2.COLOR_BGR2RGB))
+
+#cv2.imshow('image',I)
+#waitKey(0)
+
+
+
+
+
+
+plt.show()
