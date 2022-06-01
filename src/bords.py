d4c340606fa8971454aebb69f161eea6c7e486d7
AdrienHrdz <adrienhernandez16@gmail.com>
2022-06-01 11:53:47 +0200

    commit du repas

---
+++ src/bords.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from divers import LECTURE_IMAGE
+
+filename = './testBords/IMG_20220601_114342.jpg'
+
+IMAGE = LECTURE_IMAGE(filename)
+IMAGE = cv2.cvtColor(IMAGE, cv2.COLOR_RGB2GRAY)
+
+plt.imshow(IMAGE, cmap='gray')
+plt.show()
+
+[Gx,Gy] = np.gradient(IMAGE.astype(float))
+NormGrad = np.sqrt(np.square(Gx) + np.square(Gy))
+
+plt.imshow(NormGrad, cmap='gray')
+plt.colorbar()
+plt.show()
